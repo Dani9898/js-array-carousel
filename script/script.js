7652d356177ca3b87e08492b4358a0215a915bb0
Dani9898 <danieleoliva98@gmail.com>
2021-10-27 18:24:06 +0200

    creati contenuti html con js

--- script/script.js
+++ script/script.js
@@ -1,7 +1,4 @@
 
-
-
-
 const items = [
     'img/01.jpg',
     'img/02.jpg',
@@ -25,4 +22,31 @@ const text = [
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
 ]
+
+// 1) creare 5 div class item con dentro un img e un div con un h2 e un p, creare 5 div class thumb con dentro un l'img => for 
+//  il contenuto dell' img, h2, p e img è uguale al contenuto degli array in posizione corrispondente al numero del div. => Xvettore[i]
+
+const contItems = document.getElementById("items");
+const contThumbs = document.getElementById("thumbs");
+
+for (let i = 0; i < items.length; i++) {
+    contItems.innerHTML += `
+    <div class="item">
+        <img src="${items[i]}" alt="${title[i]}"></img>
+    <div class="text">
+        <h2>${title[i]}</h2>
+        <p>${text[i]}</p>
+    </div>
+    </div>`;
+    contThumbs.innerHTML += `
+    <div class="thumb"><img src="${items[i]}" alt="${title[i]}"></div>`
+}
+
+document.querySelector(".item").classList.add("active");
+document.querySelector(".thumb").classList.add("active");
+
+
+
+
+
 
